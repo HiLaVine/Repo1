2a667ff175656c85dae167958a8dc8aaed181619
HiLaVine <sinnafa01@gmail.com>
2022-12-06 01:33:18 -0600

    add string and hello world

---
+++ cadenas/src/main/java/com/mycompany/cadenas/Cadenas.java
@@ -0,0 +1,17 @@
+package com.mycompany.cadenas;
+
+/**
+ *
+ * @author HiLaVine
+ */
+public class Cadenas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        StringBuilder casa = new StringBuilder("puerta");
+     
+        
+        System.out.println(casa);
+    }
+}
